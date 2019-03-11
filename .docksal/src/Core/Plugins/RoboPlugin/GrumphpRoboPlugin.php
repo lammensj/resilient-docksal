cd64f2b90659a8883085bd5ed11dbaba1c7dab2b
Jasper Lammens <lammensjasper@gmail.com>
2019-03-11 09:44:14 +0100

    Grumphp re-init during download

--- .docksal/src/Core/Plugins/RoboPlugin/GrumphpRoboPlugin.php
+++ .docksal/src/Core/Plugins/RoboPlugin/GrumphpRoboPlugin.php
@@ -27,7 +27,7 @@ class GrumphpRoboPlugin extends AbstractRoboPlugin implements RoboPluginDownload
         $grumphpDir = sprintf(
           '%s/assets/%s/grumphp',
           $this->configFactory->get('setup_path'),
-          $this->configFactory->get('project_type'),
+          $this->configFactory->get('project_type')
         );
         if (file_exists($grumphpDir)) {
             $tasks[] = $this->taskFilesystemStack()
@@ -42,6 +42,8 @@ class GrumphpRoboPlugin extends AbstractRoboPlugin implements RoboPluginDownload
             )
               ->from('[jira_code]')
               ->to($this->configFactory->get('jira_code'));
+            $tasks[] = $this->taskExec(sprintf('%s/vendor/bin/grumphp git:deinit', $this->configFactory->get('project_root')));
+            $tasks[] = $this->taskExec(sprintf('%s/vendor/bin/grumphp git:init', $this->configFactory->get('project_root')));
         }
 
         return $tasks;
